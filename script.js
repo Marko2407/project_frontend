560b860dc8c5bc34c2e3ae84ed9583a4502ac538
Mateo Besednik <mateo.besednik@gmail.com>
2022-07-08 21:41:50 +0200

    update js

--- script.js
+++ script.js
@@ -1,57 +1,55 @@
-const workoutList = document.getElementById('workout-list')
-let list = []
-const form = document.getElementById('search')
+const workoutList = document.getElementById('workout-list');
+let list = [];
+const form = document.getElementById('search');
 const searchInput = form.elements['search'];
 
-async function getAll(){
-    const workouts = await queryFetch(`
+async function getAll() {
+  const workouts = await queryFetch(`
     query {
-        getCurrentWeekWorkouts {
-            day
-            workouts {
-              id
-              title
-              day
-              dateCreated
-              description
-            }
-          }
+        getTodayWorkouts {
+    id
+    day
+    title
+    description
+    dateCreated
+    series
+    reps
+  }
         }
-    `)
-    workouts.data.getCurrentWeekWorkouts.forEach(data => {
-      console.log(data)
-     let element = document.createElement('div')
-     element.innerText = data.day
-     workoutList.append(element) 
-     data.workouts.forEach(workouts => {
-         let element = document.createElement('div')
-         element.innerText = workouts.title
-         workoutList.append(element) 
-     })
- });
+    `);
+  console.log(workouts.data.getTodayWorkouts);
+  if (workouts.data.getTodayWorkouts != null) {
+    workouts.data.getTodayWorkouts.forEach((data) => {
+      console.log(data);
+      let element = document.createElement('div');
+      element.innerText = data.title;
+      workoutList.append(element);
+    });
+  } else {
+    console.log('Empty list');
+  }
 }
 
 //First method to initiate when page open
-getAll()
+getAll();
 
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const getWorkoutBySearchInput = await getWorkoutBySearch(searchInput.value);
+  if (getWorkoutBySearchInput.getWorkoutBySearchInput != null) {
+    workoutList.innerHTML = '';
+    getWorkoutBySearchInput.getWorkoutBySearchInput.forEach((e) => {
+      console.log(e);
+      let element = document.createElement('div');
+      element.innerText = e.title;
+      workoutList.append(element);
+    });
+  }
+});
 
-form.addEventListener('submit', async e => {
-    e.preventDefault()
-    const getWorkoutBySearchInput = await getWorkoutBySearch(searchInput.value)
-    if (getWorkoutBySearchInput.getWorkoutBySearchInput !=null){
-        workoutList.innerHTML = ""
-         getWorkoutBySearchInput.getWorkoutBySearchInput.forEach(e => {
-        console.log(e);
-        let element = document.createElement('div')
-         element.innerText = e.title
-         workoutList.append(element)
-})
-    }
-    
-})
-
-function getWorkoutBySearch(searchInput){
-    return queryFetch(`
+function getWorkoutBySearch(searchInput) {
+  return queryFetch(
+    `
          query GetWorkoutBySearchInput($title: String) {
              getWorkoutBySearchInput(title: $title) {
                id
@@ -61,18 +59,20 @@ function getWorkoutBySearch(searchInput){
                day
              }
            }
-         `, {title: searchInput})
-         .then( res => { return (res.data)})
-     }
+         `,
+    { title: searchInput }
+  ).then((res) => {
+    return res.data;
+  });
+}
 
-function queryFetch(query, variables){
-  return  fetch('http://localhost:4000/graphql/', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ 
+function queryFetch(query, variables) {
+  return fetch('http://localhost:4000/graphql/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
       query: query,
-      variables: variables
-  }),
-})
-.then(res => res.json())
+      variables: variables,
+    }),
+  }).then((res) => res.json());
 }
